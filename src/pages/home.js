81f4b2fd7b1d12c45021b11215f056dbbba40c2c
bernardoDev <bernardo.acaldas@gmail.com>
2021-09-14 20:17:23 -0300

    upload app

---
+++ src/pages/home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { 
+    View,
+    StyleSheet,
+    Text
+} from 'react-native';
+
+export function Home(){
+    return(
+        <View>
+            <Text> Home </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
