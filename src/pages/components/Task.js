51a2f1e8f6e9607a656062838f2a9f7b643ce079
bernardoDev <bernardo.acaldas@gmail.com>
2021-09-14 23:05:06 -0300

    update

--- src/pages/components/Task.js
+++ src/pages/components/Task.js
@@ -2,14 +2,58 @@ import React from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from 'react-native';
 
 
 export function Task(props){
     return (
-        <View>
-            <Text> {props.text} </Text>
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <Text> {props.text} </Text>
+
+            </View>
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#E5E5E5',
+        padding: 15,
+        borderRadius: 10,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+        
+        elevation: 6,
+
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        borderRadius: 5,
+        opacity: 0.4,
+        marginRight: 15
+
+    }
+
+});
